48ea9a5d6df86dbe32f4bf6a5c190b780afe1069
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-02 13:27:25 +0500

    Added request specs, and model tests

--- spec/requests/api_developments_spec.rb
+++ spec/requests/api_developments_spec.rb
@@ -1,10 +1,47 @@
 require 'rails_helper'
 
 RSpec.describe "ApiDevelopments", type: :request do
-  describe "GET /api_developments" do
-    it "works! (now write some real specs)" do
-      get api_developments_index_path
-      expect(response).to have_http_status(200)
+
+  def parsed_body
+    JSON.parse(response.body)
+  end
+
+  describe "RDBMS-backend" do
+    before(:each) do
+      City.delete_all
+      @object = City.create(:name => "Lahore")
+    end
+    after(:each) { City.delete_all }
+
+    it "create RDBMS-backend model" do
+      expect(City.find(@object.id).name).to eq("Lahore")
+    end
+
+    it "expose RDBMS-backend API resource" do
+      expect(cities_path).to eq("/api/cities")
+      get city_path(@object.id)
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body["name"]).to eq("Lahore")
+    end
+  end
+
+  describe "MongoDB-backend" do
+    before(:each) do
+      State.delete_all
+      @object = State.create(:name => "Pindi")
+    end
+    after(:each) { State.delete_all }
+
+    it "create MongoDB-backend model" do
+      expect(State.find(@object.id).name).to eq("Punjab")
+    end
+
+    it "expose MongoDB-backend API resource" do
+      expect(states_path).to eq("/api/states")
+      get state_path(@object.id)
+      expect(response).to have_http_status(:ok)
+      expect(parsed_body).to include("created_at")
+      expect(parsed_body).to include("id" => @object.id.to_s)
     end
   end
 end
