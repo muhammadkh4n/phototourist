251edbf807976cabcc6a79c0aee5b17953885261
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-04 14:10:07 +0500

    Added redirect

--- app/controllers/ui_controller.rb
+++ app/controllers/ui_controller.rb
@@ -1,2 +1,6 @@
 class UiController < ApplicationController
+  # def index
+  #   redirect_to "/client/index.html", :status => :moved_permanently
+  #   # redirect_to "https://muhammadkh4n.github.io/phototourist/public/client"
+  # end
 end
