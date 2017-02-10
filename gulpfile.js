711358990688dfd23ed730c861e44136e59927c3
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-10 14:26:43 +0500

    Added phototourist module

--- gulpfile.js
+++ gulpfile.js
@@ -158,7 +158,9 @@ gulp.task("run", ["build", "browserSync"], function () {
 gulp.task("dist:assets", ["build"], function () {
   return gulp.src(cfg.root_html.src).pipe(debug())
     .pipe(useref({ searchPath: devResourcePath }))
-    .pipe(gulpif(["**/*constant.js"], replace(cfg.apiUrl.dev, cfg.apiUrl.prd))) // Change urls
+    .pipe(gulpif(["**/*.js"], replace(cfg.apiUrl.dev, cfg.apiUrl.prd))) // Change urls
+    // .pipe(gulpif(["**/*constant.js"], replace(cfg.apiUrl.dev, cfg.apiUrl.prd))) // Change urls
+    // .pipe(replace(cfg.apiUrl.dev, cfg.apiUrl.prd)) // Change urls
     .pipe(gulpif(["**/*.js"], uglify())) // minify js
     .pipe(gulpif(["**/*.css"], cssMin())) // minify css
     .pipe(gulp.dest(distPath)).pipe(debug());
@@ -171,8 +173,15 @@ gulp.task("dist:fonts", function () {
 });
 
 // build/copy over html resources in to dist
+// gulp.task("dist:html", function () {
+//   return gulp.src(cfg.html.src).pipe(debug())
+//     .pipe(htmlMin({collapseWhitespace: true}))
+//     .pipe(gulp.dest(distPath)).pipe(debug());
+// });
+
+// build/copy over html resources in to dist
 gulp.task("dist:html", function () {
-  return gulp.src(cfg.html.src).pipe(debug())
+  return gulp.src(cfg.html.src, {base: srcPath+"/javascripts"}).pipe(debug())
     .pipe(htmlMin({collapseWhitespace: true}))
     .pipe(gulp.dest(distPath)).pipe(debug());
 });
