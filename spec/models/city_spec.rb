fa424c23bd604d4260cbd2025f83a91d97e25181
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-14 15:49:41 +0500

    added subjects

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 describe City, type: :model do
 
-  context "created City" do
+  context "created City (let)" do
     let(:city) { City.create(:name => "test") }
     it { expect(city).to be_persisted }
     it { expect(city.name).to eq("test") }
     it { expect(City.find(city.id)).to_not be_nil }
+  end
+
+  context "created City (subject)" do
+    subject { City.create(:name => "test") }
+    it { is_expected.to be_persisted }
+    it { expect(subject.name).to eq("test") }
+    it { expect(City.find(subject.id)).to_not be_nil }
   end
 
 end
