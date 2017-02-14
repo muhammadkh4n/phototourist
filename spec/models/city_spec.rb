7dfeb0332a2846d663486b0836cd28d03bff8aae
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-14 15:04:58 +0500

    let construct

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -3,13 +3,11 @@ require 'rails_helper'
 describe City, type: :model do
 
   context "created City" do
-    before(:each) do
-      @city=City.create(:name => "test")
-    end
+    let(:city) { City.create(:name => "test") }
     it "will be persisited, have a name, and be found" do
-      expect(@city).to be_persisted
-      expect(@city.name).to eq("test")
-      expect(City.find(@city.id)).to_not be_nil
+      expect(city).to be_persisted
+      expect(city.name).to eq("test")
+      expect(City.find(city.id)).to_not be_nil
     end
   end
 
