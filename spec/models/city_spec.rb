d0c81c0d1aaf1b50d39d76af9ee3af41ef31a448
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-14 15:45:08 +0500

    self describing matchers

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -4,17 +4,9 @@ describe City, type: :model do
 
   context "created City" do
     let(:city) { City.create(:name => "test") }
-    it "will be persisited" do
-      expect(city).to be_persisted
-    end
-
-    it "will have a name" do
-      expect(city.name).to eq("test")
-    end
-
-    it "will be found" do
-      expect(City.find(city.id)).to_not be_nil
-    end
+    it { expect(city).to be_persisted }
+    it { expect(city.name).to eq("test") }
+    it { expect(City.find(city.id)).to_not be_nil }
   end
 
 end
