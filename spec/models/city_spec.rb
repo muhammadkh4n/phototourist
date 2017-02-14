1673a3b2a5d027a93c1cd53aa94b92a3e129d4b5
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-14 15:01:57 +0500

    context and before each, global variable

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -3,11 +3,13 @@ require 'rails_helper'
 describe City, type: :model do
 
   context "created City" do
+    before(:each) do
+      @city=City.create(:name => "test")
+    end
     it "will be persisited, have a name, and be found" do
-      city=City.create(:name => "test")
-      expect(city).to be_persisted
-      expect(city.name).to eq("test")
-      expect(City.find(city.id)).to_not be_nil
+      expect(@city).to be_persisted
+      expect(@city.name).to eq("test")
+      expect(City.find(@city.id)).to_not be_nil
     end
   end
 
