13702e89ec1e96f6ff0440649a57a33cd6ef7167
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-14 14:57:28 +0500

    logger config

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 describe City, type: :model do
 
-  context "valid City" do
-    it "has a name" do
-      city = City.create(:name => "test")
-      expect(city).to be_valid
-      expect(city.name).to_not be_nil
+  context "created City" do
+    it "will be persisited, have a name, and be found" do
+      city=City.create(:name => "test")
+      expect(city).to be_persisted
+      expect(city.name).to eq("test")
+      expect(City.find(city.id)).to_not be_nil
     end
   end
 
