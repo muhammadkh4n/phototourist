30734a9afca89d3948046841916e6cf0ca286288
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-14 15:41:21 +0500

    isolated test blocks

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -4,9 +4,15 @@ describe City, type: :model do
 
   context "created City" do
     let(:city) { City.create(:name => "test") }
-    it "will be persisited, have a name, and be found" do
+    it "will be persisited" do
       expect(city).to be_persisted
+    end
+
+    it "will have a name" do
       expect(city.name).to eq("test")
+    end
+
+    it "will be found" do
       expect(City.find(city.id)).to_not be_nil
     end
   end
