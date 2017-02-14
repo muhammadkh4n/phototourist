80e3eb21754f4df15881bc3f50a0291468ee4939
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-14 20:44:36 +0500

    Added places factory and some blocks to it

--- spec/factories/places.rb
+++ spec/factories/places.rb
@@ -4,6 +4,36 @@ FactoryGirl.define do
     name "test"
   end
 
+  factory :city_sequence, class: 'City' do
+    sequence(:name) { |n| "test#{n}" }
+  end
+
+  factory :city_names, class: 'City' do
+    sequence(:name) { |n| ["Larry", "Chooti", "Qismat"][n%3] }
+  end
+
+  factory :city_transient, class: 'City' do
+    name "test"
+    transient do
+      male true
+    end
+
+    after(:build) do |object, props|
+      object.name = props.male ? "Mr. Test" : "Ms. Test"
+    end
+  end
+
+  factory :city_faker, class: 'City' do
+    name { Faker::Address.city }
+  end
+
+  factory :city_ctor, class: 'City' do
+    transient do
+      hash {}
+    end
+    initialize_with { City.new(hash) }
+  end
+
   factory :city, :parent => :city_fixed do
   end
 
