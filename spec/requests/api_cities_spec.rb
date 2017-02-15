7dcda2d5f8ad4b3abf87d23637e70e40786fb9a6
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-15 18:49:30 +0500

    Added Api test for a city object

--- spec/requests/api_cities_spec.rb
+++ spec/requests/api_cities_spec.rb
@@ -18,4 +18,41 @@ describe "City API", type: :request do
       expect(payload.map{|c|c["name"]}).to eq(cities.map{|c|c[:name]})
     end
   end
+
+  context "a specific City exists" do
+    let(:city) { FactoryGirl.create(:city) }
+    let(:bad_id) { 123453421 }
+
+    it "returns City when using correct ID" do
+      get city_path(city.id)
+      expect(response).to have_http_status(:ok)
+      #pp parsed_body
+
+      payload=parsed_body
+      expect(payload).to have_key("id")
+      expect(payload).to have_key("name")
+      expect(payload["id"]).to eq(city.id)
+      expect(payload["name"]).to eq(city.name)
+    end
+
+    it "returns not found when using incorrect ID" do
+      get city_path(bad_id)
+      expect(response).to have_http_status(:not_found)
+      expect(response.content_type).to eq("application/json")
+
+      payload=parsed_body
+      expect(payload).to have_key("errors")
+      expect(payload["errors"]).to have_key("full_messages")
+      expect(payload["errors"]["full_messages"][0]).to include("cannot", "#{bad_id}")
+    end
+  end
+
+  context "create a new City" do
+    it "can create with provided name"
+  end
+
+  context "existing City" do
+    it "can update name"
+    it "can be deleted"
+  end
 end
