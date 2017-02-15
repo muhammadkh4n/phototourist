6b128f7cade7f24f9de5257cfd443ed4ae08bbc3
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-15 17:52:51 +0500

    Added a test to City API spec

--- spec/requests/api_cities_spec.rb
+++ spec/requests/api_cities_spec.rb
@@ -3,10 +3,19 @@ require 'rails_helper'
 describe "City API", type: :request do
   include_context "db_cleanup_each", :transaction
 
-  describe "GET /api/cities" do
-    it "works! (now write some real specs)" do
-      get cities_path
+  context "caller requests list of Foos" do
+    let!(:cities) { (1..5).map {|i| FactoryGirl.create(:city) } }
+
+    it "return all instances" do
+      get cities_path, {:s=>"search"}, {"Accept"=>"application/json"}
+      expect(request.method).to eq("GET")
       expect(response).to have_http_status(:ok)
+      expect(response.content_type).to eq("application/json")
+      expect(response["X-Frame-Options"]).to eq("SAMEORIGIN")
+
+      payload=JSON.parse(response.body)
+      expect(payload.count).to eq(cities.count)
+      expect(payload.map{|c|c["name"]}).to eq(cities.map{|c|c[:name]})
     end
   end
 end
