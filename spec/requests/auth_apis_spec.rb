ce40f258592c3810a60be2b2a3270a3dce2800ef
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-26 19:43:50 +0500

    Added authn controller, access-token tests

--- spec/requests/auth_apis_spec.rb
+++ spec/requests/auth_apis_spec.rb
@@ -58,15 +58,59 @@ RSpec.describe "Authentication Api", type: :request do
   end
 
   context "anonymous user" do
-    it "accesses unprotected"
-    it "fails to access protected resource"
+    it "accesses unprotected" do
+      get authn_whoami_path
+      expect(response).to have_http_status(:ok)
+
+      expect(parsed_body).to eq({})
+    end
+    it "fails to access protected resource" do
+      get authn_checkme_path
+      expect(response).to have_http_status(:unauthorized)
+      
+      expect(parsed_body).to include("errors"=>["Authorized users only."])
+    end
   end
 
   context "login" do
+    let(:account) { signup user_props, :ok }
+    let!(:user) { login account, :ok }
     context "valid user login" do
-      it "generates access token"
-      it "grants access to resource"
-      it "grants access to resource multiple times"
+
+      it "generates access token" do
+        expect(response.headers).to include("uid"=>account[:uid])
+        expect(response.headers).to include("access-token")
+        expect(response.headers).to include("client")
+        expect(response.headers).to include("token-type"=>"Bearer")
+      end
+
+      it "extracts access headers" do
+        expect(access_tokens?).to be true
+
+        expect(access_tokens).to include("uid"=>account[:uid])
+        expect(access_tokens).to include("access-token")
+        expect(access_tokens).to include("client")
+        expect(access_tokens).to include("token-type"=>"Bearer")
+      end
+      
+      it "grants access to resource" do
+        get authn_checkme_path, access_tokens
+        expect(response).to have_http_status(:ok)
+
+        payload=parsed_body
+        expect(payload).to include("id"=>account[:id])
+        expect(payload).to include("uid"=>account[:uid])
+      end
+      
+      it "grants access to resource multiple times" do
+        (1..10).each do |i|
+          # puts i
+          #quick calls < 5sec use same tokens
+          get authn_checkme_path, access_tokens
+          expect(response).to have_http_status(:ok)
+        end
+      end
+      
       it "logout"
     end
 
