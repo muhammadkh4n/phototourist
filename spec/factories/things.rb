5decf3c7c303c88fdbabc364a5b4480c09998d2c
Muhammad Khan <muhammadkh4n@gmail.com>
2017-02-28 17:46:17 +0500

    added thing_image model and tested it

--- spec/factories/things.rb
+++ spec/factories/things.rb
@@ -8,5 +8,14 @@ FactoryGirl.define do
       description { Faker::Lorem.paragraphs.join }
       notes       { Faker::Lorem.paragraphs.join }
     end
+
+    trait :with_image do
+      transient do
+        image_count 1
+      end
+      after(:build) do |thing, props|
+        thing.thing_images << build_list(:thing_image, props.image_count, thing: thing)
+      end
+    end
   end
 end
