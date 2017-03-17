e513680dc861bd6ff180c0a076e69fe6bc010c8e
Muhammad Khan <muhammadkh4n@gmail.com>
2017-03-17 15:20:11 +0500

    image size from exifr gem

--- app/models/image_content.rb
+++ app/models/image_content.rb
@@ -1,4 +1,6 @@
 class ImageContent
+  CONTENT_TYPES=["image/jpeg", "image/jpg"]
+  
   include Mongoid::Document
   field :image_id, type: Integer
   field :width, type: Integer
@@ -6,4 +8,38 @@ class ImageContent
   field :content_type, type: String
   field :content, type: BSON::Binary
   field :original, type: Mongoid::Boolean
+
+  def content=(value)
+    if self[:content]
+      self.width = nil
+      self.height = nil
+    end
+    self[:content]=self.class.to_binary(value)
+    exif.tap do |xf|
+      self.width = xf.width   if xf
+      self.height = xf.height if xf
+    end
+  end
+    
+  def self.to_binary(value)
+    case
+    when value.is_a?(IO) || value.is_a?(StringIO)
+      value.rewind
+      BSON::Binary.new(value.read)
+    when value.is_a?(BSON::Binary)
+      value
+    when value.is_a?(String)
+      decoded=Base64.decode64(value)
+      BSON::Binary.new(decoded)
+    end
+  end
+
+  def exif
+    if content
+      case
+      when (CONTENT_TYPES.include? content_type)
+        EXIFR::JPEG.new(StringIO.new(content.data))
+      end
+    end
+  end
 end
--- spec/models/image_content_spec.rb
+++ spec/models/image_content_spec.rb
@@ -10,26 +10,82 @@ RSpec.describe "ImageContent", type: :model do
   
   context "BSON::Binary" do
     it "demonstrates BSON::Binary" do
-      fin
+      binary=BSON::Binary.new(fin.read)
+      expect(binary.data.size).to eq(fin.size)
+
+      fout = File.open("tmp/out.jpg", "wb")
+      fout.write(binary.data)
+      expect(fout.size).to eq(fin.size)
+      fout.close
     end
     
     context "using helper" do
-      it "derives BSON::Binary from file"
-      it "derives BSON::Binary from StringIO"
-      it "derives BSON::Binary from BSON::Binary"
-      it "derives BSON::Binary from base64 encoded String"
+      it "derives BSON::Binary from file" do
+        binary = ImageContent.to_binary(fin)
+        expect(binary.class).to be(BSON::Binary)
+        expect(binary.data.size).to eq(fin.size)
+      end
+      it "derives BSON::Binary from StringIO" do
+        strio = StringIO.new(fin.read)
+        binary = ImageContent.to_binary(strio)
+        expect(binary.class).to be(BSON::Binary)
+        expect(binary.data.size).to eq(fin.size)
+      end
+      it "derives BSON::Binary from BSON::Binary" do
+        binary=BSON::Binary.new(fin.read)
+        binary=ImageContent.to_binary(binary)
+        expect(binary.class).to be(BSON::Binary)
+        expect(binary.data.size).to eq(fin.size)
+      end
+      it "derives BSON::Binary from base64 encoded String" do
+        content=BSON::Binary.new(fin.read)
+        b64=Base64.encode64(content.data)
+        
+        binary=ImageContent.to_binary(b64)
+        expect(binary.class).to be(BSON::Binary)
+        expect(binary.data.size).to eq(fin.size)
+      end
     end
   end
 
   context "assign content" do
-    it "sets content"
-    it "mass-assigns content"
+    it "sets content" do
+      strio = StringIO.new(fin.read)
+      ic = ImageContent.new
+      ic.content = strio
+      expect(ic.content.class).to be(BSON::Binary)
+      expect(ic.content.data.size).to eq(fin.size)
+    end
+    it "mass-assigns content" do
+      strio = StringIO.new(fin.read)
+      ic = ImageContent.new(:content => strio)
+      expect(ic.content.class).to be(BSON::Binary)
+      expect(ic.content.data.size).to eq(fin.size)
+    end
   end
 
   context "set size from JPEG" do
-    it "reads EXIF from JPEG"
-    it "sets size from EXIF"
-    it "sets size manually"
+    let(:ic) { ImageContent.new(:content_type => "image/jpg", :content => fin) }
+    
+    it "reads EXIF from JPEG" do
+      expect(exif=ic.exif).to_not be_nil
+      expect(exif.width).to_not be_nil
+      expect(exif.height).to_not be_nil
+    end
+    it "sets size from EXIF" do
+      expect(ic.width).to eq(ic.exif.width)
+      expect(ic.height).to eq(ic.exif.height)
+    end
+    it "sets size manually" do
+      width = 666; height = 444;
+      ic = ImageContent.new(
+        width: width,
+        height: height,
+        content_type: "image/png",
+        content: fin)
+      expect(ic.width).to eq(width)
+      expect(ic.height).to eq(height)
+    end
   end
 
   context "valid image content" do
