143559202a662949aaae183939f3e89bc302c6f9
Muhammad Khan <muhammadkh4n@gmail.com>
2017-04-07 19:42:28 +0500

    current loc jhu config

--- app/assets/javascripts/spa/geoloc/jhu_location.config.js
+++ app/assets/javascripts/spa/geoloc/jhu_location.config.js
@@ -4,8 +4,13 @@
   angular
     .module("spa.geoloc")
     .config(JhuLocationOverride);
-	
-  JhuLocationOverride.$inject=[];
-  function JhuLocationOverride() {
+
+	JhuLocationOverride.$inject=["spa.geoloc.myLocationProvider"];
+  function JhuLocationOverride(myLocationProvider) {
+    myLocationProvider.usePositionOverride({
+      longitude:-76.6200464, 
+      latitude: 39.3304957      
+    });
   }
+	
 })();
