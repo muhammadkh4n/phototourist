26337c45d76257a91a2679fbe29a2d2ab46e413b
Muhammad Khan <muhammadkh4n@gmail.com>
2017-04-25 20:58:34 +0500

    added current subjects images things components

---
+++ app/assets/javascripts/spa/subjects/current_images/current_images.component.js
@@ -0,0 +1,28 @@
+(function() {
+  "use strict";
+
+  angular
+    .module("spa.subjects")
+    .component("sdCurrentImages", {
+      templateUrl: imagesTemplateUrl,
+      controller: CurrentImagesController,
+    });
+
+
+  imagesTemplateUrl.$inject = ["spa.config.APP_CONFIG"];
+  function imagesTemplateUrl(APP_CONFIG) {
+    return APP_CONFIG.current_images_html;
+  }
+
+  CurrentImagesController.$inject = ["$scope",
+                                     "spa.subjects.currentSubjects"];
+  function CurrentImagesController($scope, currentSubjects) {
+    var vm=this;
+
+    vm.$onInit = function() {
+      console.log("CurrentImagesController",$scope);
+    }
+    return;
+    //////////////
+  }
+})();
