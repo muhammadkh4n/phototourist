2211f2891c34d355b8fc6628db28858f13e04826
Muhammad Khan <muhammadkh4n@gmail.com>
2017-04-25 18:12:22 +0500

    subjects default state

--- app/assets/javascripts/spa/app.router.js
+++ app/assets/javascripts/spa/app.router.js
@@ -1,38 +1,38 @@
-(function () {
+(function() {
   "use strict";
 
-  angular.module("spa")
-    .config(RouterConfig);
+  angular
+    .module("spa")
+    .config(RouterFunction);
 
-  RouterConfig.$inject = ["$stateProvider", "$urlRouterProvider", "spa.config.APP_CONFIG"];
-  function RouterConfig($stateProvider, $urlRouterProvider, APP_CONFIG) {
+  RouterFunction.$inject = ["$stateProvider",
+                            "$urlRouterProvider",
+                            "spa.config.APP_CONFIG"];
+
+  function RouterFunction($stateProvider, $urlRouterProvider, APP_CONFIG) {
     $stateProvider
-			.state("home", {
-	      url: "/",
-	      templateUrl: APP_CONFIG.main_page_html
-			})
-			.state("accountSignup", {
-				url: "/signup",
-				templateUrl: APP_CONFIG.signup_page_html
-			})
-			.state("authn", {
-				url: "/authn",
-				templateUrl: APP_CONFIG.authn_page_html
-			})
-			.state("images",{
-				url: "/images/:id",
-				templateUrl: APP_CONFIG.images_page_html
-			})
-			.state("things",{
-				url: "/things/:id",
-				templateUrl: APP_CONFIG.things_page_html
-			})
-      .state("subjects",{
-       url: "/subjects",
-       templateUrl: APP_CONFIG.subjects_page_html
-      })
-		;
+    .state("home",{
+      url: "/subjects",
+      templateUrl: APP_CONFIG.subjects_page_html
+    })
+    .state("accountSignup",{
+      url: "/signup",
+      templateUrl: APP_CONFIG.signup_page_html
+    })
+    .state("authn",{
+      url: "/authn",
+      templateUrl: APP_CONFIG.authn_page_html
+    })
+    .state("images",{
+      url: "/images/:id",
+      templateUrl: APP_CONFIG.images_page_html
+    })
+    .state("things",{
+      url: "/things/:id",
+      templateUrl: APP_CONFIG.things_page_html
+    })
+    ;
 
-    // $urlRouterProvider.otherwise("/");
+    $urlRouterProvider.otherwise("/subjects");
   }
 })();
