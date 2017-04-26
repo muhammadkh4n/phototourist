cc9b2c8967407cc58cf913990ae82975a8e13699
Muhammad Khan <muhammadkh4n@gmail.com>
2017-04-26 19:57:49 +0500

    map current origin

--- app/assets/javascripts/spa/geoloc/map.service.js
+++ app/assets/javascripts/spa/geoloc/map.service.js
@@ -10,7 +10,7 @@
 
     function GeolocMap(element, mapOptions) {
       var service=this;
-      service.options = {}
+      service.options = {};
       service.markers = [];
       service.currentMarker = null;
       service.options = service.normalizeMapOptions(mapOptions);
@@ -27,7 +27,21 @@
     };
 
     GeolocMap.prototype.center = function(mapOptions) {
-      //...
+      if (this.map && mapOptions) {
+        this.normalizeMapOptions(mapOptions);
+        if (mapOptions.center) {
+          this.options.center=mapOptions.center;
+          this.map.setCenter(this.options.center);
+        }
+        if (mapOptions.zoom) {
+          this.options.zoom = mapOptions.zoom;
+          this.map.setZoom(this.options.zoom);
+        }
+        if (mapOptions.mapTypeId) {
+          this.options.mapTypeId = mapOptions.mapTypeId;
+          this.map.setMapTypeId(this.options.mapTypeId);
+        }
+      }
     };
 
     GeolocMap.prototype.getMarkers = function() {
@@ -44,6 +58,16 @@
       });
       this.markers = [];
     }
+    GeolocMap.prototype.clearOriginMarker = function() {
+      var m = this.originMarker;
+      if (m) {
+        google.maps.event.removeListener(m.listener);
+        m.marker.setMap(null);
+      };
+      if (m === this.originMarker) {
+        this.originMarker=null;
+      }
+    }
 
     GeolocMap.prototype.displayMarker = function(markerOptions) {
       if (!this.map) { return; }
@@ -58,6 +82,7 @@
       var service=this;
       var infoWindow=new google.maps.InfoWindow({content: markerOptions.content});
       var listener=marker.addListener('click', function(){
+        console.log("map listener called");
         service.setActiveMarker(markerOptions);
         $timeout();
       });
@@ -86,17 +111,21 @@
         content = "Origin";
       }
 
-      this.originMarker = this.displayMarker({
-            position: this.options.center,
-            title: "origin",
-            icon: APP_CONFIG.origin_marker,
-            content: content
+      var service = this;
+      $timeout(function(){
+        service.clearOriginMarker();
+        service.originMarker = service.displayMarker({
+              position: service.options.center,
+              title: "origin",
+              icon: APP_CONFIG.origin_marker,
+              content: content
+        });
       });
     }
 
     GeolocMap.prototype.setActiveMarker = function(markerOptions) {
       console.log("setting new marker new/old:", markerOptions, this.currentMarker);
-      if (this.currentMarker) {
+      if (this.currentMarker && markerOptions !== this.currentMarker) {
         this.currentMarker.infoWindow.close();
       }
       if (markerOptions && markerOptions.infoWindow) {
